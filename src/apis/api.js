60df2f87ca9262d3f597311c3982a1d8a3686946
Alexander Leyva <cyberxander90@gmail.com>
2019-03-03 22:46:23 -0300

    Allow fetch products

---
+++ src/apis/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+import {
+  BASE_API_URL
+} from '../constant';
+
+export default axios.create({
+  baseURL: BASE_API_URL
+});
--- src/components/products/product-list.jsx
+++ src/components/products/product-list.jsx
@@ -3,46 +3,62 @@ import React from 'react';
 import './product-list.scss';
 import ProductListItem from 'components/products/product-list-item';
 import EmptyResults from 'components/empty-results';
+import { searchProducts } from 'adapters/products';
 
-function ProductList() {
-  const products = [
-    {
-      id: 1,
-      name: 'hola',
-      description: 'este esta buenisimo',
-      imageUrl:
-        'http://mla-s2-p.mlstatic.com/871062-MLA28229293206_092018-I.jpg',
-      location: 'Habana',
-      price: 120,
-      isFreeShipping: true
-    },
-    {
-      id: 2,
-      name: 'hola',
-      description: 'este esta buenisimo',
-      imageUrl:
-        'http://mla-s2-p.mlstatic.com/871062-MLA28229293206_092018-I.jpg',
-      location: 'Habana',
-      price: 120,
-      isFreeShipping: false
-    }
-  ];
+class ProductList extends React.Component {
+  constructor() {
+    super(...arguments);
 
-  return (
-    <ul className="product-list">
-      {products.length > 0 ? (
-        products.map(product => (
-          <li className="product-list__item" key={product.id}>
-            <ProductListItem {...product} />
+    this.state = {
+      products: [
+        {
+          id: 1,
+          name: 'hola',
+          description: 'este esta buenisimo',
+          imageUrl:
+            'http://mla-s2-p.mlstatic.com/871062-MLA28229293206_092018-I.jpg',
+          location: 'Habana',
+          price: 120,
+          isFreeShipping: true
+        },
+        {
+          id: 2,
+          name: 'hola',
+          description: 'este esta buenisimo',
+          imageUrl:
+            'http://mla-s2-p.mlstatic.com/871062-MLA28229293206_092018-I.jpg',
+          location: 'Habana',
+          price: 120,
+          isFreeShipping: false
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    console.log(process.env);
+    searchProducts('mochilas').then(products => this.setState(products));
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ul className="product-list">
+        {products.length > 0 ? (
+          products.map(product => (
+            <li className="product-list__item" key={product.id}>
+              <ProductListItem {...product} />
+            </li>
+          ))
+        ) : (
+          <li>
+            <EmptyResults />
           </li>
-        ))
-      ) : (
-        <li>
-          <EmptyResults />
-        </li>
-      )}
-    </ul>
-  );
+        )}
+      </ul>
+    );
+  }
 }
 
 export default ProductList;
