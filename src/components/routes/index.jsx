c41f79f0bec8c5a62862aa41e77a7cd25b135911
Alexander Leyva <cyberxander90@gmail.com>
2019-03-04 11:47:18 -0300

    Code splitting with React Loadable

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
-import HomePage from 'components/routes/home-page';
-import ProductListPage from 'components/routes/product-list-page';
-import productDetailsPage from 'components/routes/product-details-page';
+const LoadableHomePage = Loadable({
+  loader: () => import('components/routes/home-page'),
+  loading: () => null
+});
+
+const LoadableProductListPage = Loadable({
+  loader: () => import('components/routes/product-list-page'),
+  loading: () => null
+});
+
+const LoadableProductDetailsPage = Loadable({
+  loader: () => import('components/routes/product-details-page'),
+  loading: () => null
+});
 
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/items" component={ProductListPage} />
-      <Route exact path="/item/:id" component={productDetailsPage} />
+      <Route exact path="/" component={LoadableHomePage} />
+      <Route exact path="/items" component={LoadableProductListPage} />
+      <Route exact path="/item/:id" component={LoadableProductDetailsPage} />
     </Switch>
   );
 }
